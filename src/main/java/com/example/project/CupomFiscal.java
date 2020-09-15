9561aa67e1db1bb7b12ebf9af003d6952f52e205
whendev <whendel.morais.9@gmail.com>
2020-09-15 19:44:03 -0300

    exercise solved

--- src/main/java/com/example/project/CupomFiscal.java
+++ src/main/java/com/example/project/CupomFiscal.java
@@ -16,7 +16,18 @@ public class CupomFiscal {
 	public static String INSCRICAO_ESTADUAL = "244.898.500.113";
 
 	public static void main(final String[] args) {
-		//Implemente aqui
+		showStoreData();
+	}
+
+	public static void showStoreData()
+	{
+		System.out.printf("%s\n",NOME_LOJA);
+		System.out.printf("%s, %d %s\n",LOGRADOURO,NUMERO,COMPLEMENTO);
+		System.out.printf("%s - %s - %s\n",BAIRRO,MUNICIPIO,ESTADO);
+		System.out.printf("CEP: %s | Tel: %s\n",CEP,TELEFONE);
+		System.out.printf("%s\n",OBSERVACAO);
+		System.out.printf("CNPJ: %s\n",CNPJ);
+		System.out.printf("IE: %s\n",INSCRICAO_ESTADUAL);
 	}
 
 }
--- src/test/java/com/example/project/TestExercicio.java
+++ src/test/java/com/example/project/TestExercicio.java
@@ -15,7 +15,7 @@ public class TestExercicio {
 		rodarTestarOutput("Arcos Dourados Com. de Alimentos LTDA" + BREAK + 
 				"Av. Projetada Leste, 500 EUC F32/33/34" + BREAK + 
 				"Br. Sta Genebra - Campinas - SP" + BREAK + 
-				"CEP:13080-395 Tel (19) 3756-7408" + BREAK + 
+				"CEP: 13080-395 | Tel: (19) 3756-7408" + BREAK +
 				"Loja 1317 (PDP)" + BREAK + 
 				"CNPJ: 42.591.651/0797-34" + BREAK + 
 				"IE: 244.898.500.113" + BREAK);
@@ -39,7 +39,7 @@ public class TestExercicio {
 		rodarTestarOutput("" + BREAK + 
 				", 0 " + BREAK + 
 				" -  - " + BREAK + 
-				"CEP: Tel " + BREAK + 
+				"CEP:  | Tel: " + BREAK +
 				"" + BREAK + 
 				"CNPJ: " + BREAK + 
 				"IE: " + BREAK);
@@ -48,21 +48,27 @@ public class TestExercicio {
 	@Test
 	public void exercicio02_Customizado() {
 		//Defina seus próprios valores para as variáveis a seguir 
-		CupomFiscal.NOME_LOJA = "";
-		CupomFiscal.LOGRADOURO = "";
-		CupomFiscal.NUMERO = 0;
+		CupomFiscal.NOME_LOJA = "LOJAS AMERICANAS S.A.";
+		CupomFiscal.LOGRADOURO = "R SACADURA CABRAL";
+		CupomFiscal.NUMERO = 102;
 		CupomFiscal.COMPLEMENTO = "";
-		CupomFiscal.BAIRRO = "";
-		CupomFiscal.MUNICIPIO = "";
-		CupomFiscal.ESTADO = "";
-		CupomFiscal.CEP = "";
-		CupomFiscal.TELEFONE = "";
-		CupomFiscal.OBSERVACAO = "";
-		CupomFiscal.CNPJ = "";
-		CupomFiscal.INSCRICAO_ESTADUAL = "";
+		CupomFiscal.BAIRRO = "GAMBOA";
+		CupomFiscal.MUNICIPIO = "RIO DE JANEIRO";
+		CupomFiscal.ESTADO = "RJ";
+		CupomFiscal.CEP = "20.221-160";
+		CupomFiscal.TELEFONE = "(21) 2206-6708";
+		CupomFiscal.OBSERVACAO = "47.11-3-02 – Comércio varejista de mercadorias em geral";
+		CupomFiscal.CNPJ = "33.014.556/0001-96";
+		CupomFiscal.INSCRICAO_ESTADUAL = "85.687.08-5";
 		
 		//E atualize o texto esperado abaixo
-		rodarTestarOutput("" + BREAK);
+		rodarTestarOutput("LOJAS AMERICANAS S.A." + BREAK +
+				"R SACADURA CABRAL, 102 " + BREAK +
+				"GAMBOA - RIO DE JANEIRO - RJ" + BREAK +
+				"CEP: 20.221-160 | Tel: (21) 2206-6708" + BREAK +
+				"47.11-3-02 – Comércio varejista de mercadorias em geral" + BREAK +
+				"CNPJ: 33.014.556/0001-96" + BREAK +
+				"IE: 85.687.08-5" + BREAK);
 	}
 
 	private void rodarTestarOutput(String expected) {
